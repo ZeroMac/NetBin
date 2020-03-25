5b56eb1dc9d4d215dfe209df79dfb45c1cf852eb
Mac <380753814@qq.com>
2020-03-25 17:39:05 +0800

    单线程socket

---
+++ NetBinSocketClient/NetBinSocketClient.cpp
@@ -0,0 +1,92 @@
+﻿// NetBinSocketClient.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include<winsock.h>
+#pragma comment(lib,"ws2_32.lib")
+using namespace std;
+void initialization();
+int main()
+{
+	//定义长度变量
+	int send_len = 0;
+	int recv_len = 0;
+	//定义发送缓冲区和接受缓冲区
+	char send_buf[100];
+	char recv_buf[100];
+	//定义服务端套接字，接受请求套接字
+	SOCKET s_server;
+	//服务端地址客户端地址
+	SOCKADDR_IN server_addr;
+	initialization();
+	//填充服务端信息
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
+	server_addr.sin_port = htons(5010);
+	//创建套接字
+	s_server = socket(AF_INET, SOCK_STREAM, 0);
+	if (connect(s_server, (SOCKADDR*)&server_addr, sizeof(SOCKADDR)) == SOCKET_ERROR) {
+		cout << "服务器连接失败！" << endl;
+		WSACleanup();
+	}
+	else {
+		cout << "服务器连接成功！" << endl;
+	}
+
+	//发送,接收数据
+	while (1) {
+		cout << "请输入发送信息:";
+		cin >> send_buf;
+		send_len = send(s_server, send_buf, 100, 0);
+		if (send_len < 0) {
+			cout << "发送失败！" << endl;
+			break;
+		}
+		recv_len = recv(s_server, recv_buf, 100, 0);
+		if (recv_len < 0) {
+			cout << "接受失败！" << endl;
+			break;
+		}
+		else {
+			cout << "服务端信息:" << recv_buf << endl;
+		}
+
+	}
+	//关闭套接字
+	closesocket(s_server);
+	//释放DLL资源
+	WSACleanup(); 
+    std::cout << "Hello World!\n";
+}
+
+void initialization() {
+	//初始化套接字库
+	WORD w_req = MAKEWORD(2, 2);//版本号
+	WSADATA wsadata;
+	int err;
+	err = WSAStartup(w_req, &wsadata);
+	if (err != 0) {
+		cout << "初始化套接字库失败！" << endl;
+	}
+	else {
+		cout << "初始化套接字库成功！" << endl;
+	}
+	//检测版本号
+	if (LOBYTE(wsadata.wVersion) != 2 || HIBYTE(wsadata.wHighVersion) != 2) {
+		cout << "套接字库版本号不符！" << endl;
+		WSACleanup();
+	}
+	else {
+		cout << "套接字库版本正确！" << endl;
+	}
+}
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
